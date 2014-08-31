4147ddfbb0aaa9eef832e74b596339f215d17220
Alan Foster <alan@alanfoster.me>
2014-08-31 19:57:24 +0100

    Add formatting for blog post previews

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ Comment.delete_all
 
 first_blog = Post.create title: 'My Post Title', content: 'My Content'
 first_blog.comments.create(name: 'Alan', content: 'Comment', website: 'http://example.com')
+
+# Create random posts
+1.upto(15) do
+  Post.create title: Faker::Lorem.word, content: Faker::Lorem.paragraph
+end

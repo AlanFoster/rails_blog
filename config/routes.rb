266f46ca842505f5750d489a8a4dce424300408e
Alan Foster <alan@alanfoster.me>
2014-08-31 22:04:11 +0100

    Add about page. Update reset file to only consider h1-h6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :comments
+  get 'about' => 'about#show'
   root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.

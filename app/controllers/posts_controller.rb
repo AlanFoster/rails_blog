1d693ae4ccb6a34cc87a94518a439ff353614a79
Alan Foster <alan@alanfoster.me>
2014-10-01 00:35:54 +0100

    Add seeding mechanism for old website data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+    @posts = Post.paginate(page: params[:page], per_page: 3).order(created_at: :desc)
   end
 
   def overview
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,72 @@
-Post.delete_all
-Comment.delete_all
-User.delete_all
+require 'active_model'
 
-first_user = User.create email: 'alan@example.com', password: 'password'
+def delete_all
+  models = %i(
+    Post
+    Comment
+    User
+  )
 
-first_blog = Post.create title: 'My Post Title', content: 'My Content'
-first_blog.comments.create(name: 'Alan', content: 'Comment', website: 'http://example.com')
-
-# Create random posts
-1.upto(15) do
-  Post.create title: Faker::Lorem.word, content: Faker::Lorem.paragraph, created_at: Date.today - Faker::Number.number(2).to_i.months
+  models.each do |model|
+    Object.const_get(model).delete_all
+  end
 end
+
+def seed_users
+  User.create email: 'alan@example.com', password: 'password'
+end
+
+def seed_posts
+  field_mappings = {
+    id: :id,
+    title: :title,
+    content: :content,
+    created_at: :date
+  }
+
+  seed_old_model :Post, field_mappings
+end
+
+def seed_comments
+  field_mappings = {
+    id: :postid,
+    post_id: :id,
+    name: :name,
+    website: :website,
+    content: :comment,
+    created_at: :date
+  }
+
+  seed_old_model :Comment, field_mappings
+end
+
+def seed_old_model(model_name, field_mappings)
+  seed_data = load_seed_data("#{model_name.downcase}s")
+  seed_data.map(&:with_indifferent_access).each do |model_data|
+    model_attributes = create_model_attributes(field_mappings, model_data)
+    Object.const_get(model_name).create model_attributes
+  end
+end
+
+def create_model_attributes(field_mappings, model)
+  field_mappings.inject({}) do |mapping, (new_field, old_field)|
+    mapping[new_field] = model[old_field]
+    mapping
+  end
+end
+
+def load_seed_data(name)
+  path = File.join Rails.root,
+                   'db',
+                   'seed_data',
+                   "old_#{name}.yml"
+
+  YAML::load_file(path)
+end
+
+# Begin Seeding
+delete_all
+seed_users
+seed_posts
+seed_comments
 

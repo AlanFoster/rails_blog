478ec35d5e3167e5f9b1da4ed21416d5f5cfd8e2
Alan Foster <alan@alanfoster.me>
2014-10-02 23:30:05 +0100

    add comment model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 # Note - Ensure that the correct sanitization is used when taking user input
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  validates :name, presence: true, length: { in: 3..30 }
+  validates :content, presence: true, length: { in: 3..5000 }
+  validates :website, presence: true, length: { in: 3..30 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 describe Comment do
-  describe 'name' do
-    it { expect((Comment.new name: 'Alan').name).to eql 'Alan'}
+  context 'setting values' do
+    describe 'name' do
+      it { expect((Comment.new name: 'Alan').name).to eql 'Alan'}
+    end
+
+    describe 'content' do
+      it { expect((Comment.new content: 'Comment Content').content).to eql 'Comment Content'}
+    end
+
+    describe 'website' do
+      it { expect((Comment.new website: 'http://example.com').website).to eql 'http://example.com'}
+    end
+
+    describe 'created_at' do
+      let(:creation_time) { DateTime.now }
+      it { expect((Comment.new created_at: creation_time).created_at).to eql creation_time}
+    end
   end
 
-  describe 'content' do
-    it { expect((Comment.new content: 'Comment Content').content).to eql 'Comment Content'}
-  end
+  context 'validation' do
+    let(:comment_model) { subject }
 
-  describe 'website' do
-    it { expect((Comment.new website: 'http://example.com').website).to eql 'http://example.com'}
-  end
+    it { expect(comment_model).to validate_presence_of(:name) }
+    it { expect(comment_model).to validate_presence_of(:content) }
+    it { expect(comment_model).to validate_presence_of(:website) }
 
-  describe 'created_at' do
-    let(:creation_time) { DateTime.now }
-    it { expect((Comment.new created_at: creation_time).created_at).to eql creation_time}
+    it { expect(comment_model).to ensure_length_of(:name).is_at_least(3).is_at_most(30) }
+    it { expect(comment_model).to ensure_length_of(:content).is_at_least(3).is_at_most(5000) }
+    it { expect(comment_model).to ensure_length_of(:website).is_at_least(3).is_at_most(30) }
+
+    it { expect(comment_model).to belong_to(:post) }
   end
 end

f28d4f0893643657b0d9699945dbb36214b1042e
Alan Foster <alan@alanfoster.me>
2014-10-12 03:06:14 +0100

    Update post form to post/update to the correct path

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class PostsController < Admin::BaseController
-    before_action :set_post, only: [:show, :edit, :update]
+    before_action :set_post, only: [:edit, :update]
 
     def index
       @posts = Post.paginate(page: params[:page], per_page: 5).order(created_at: :desc)

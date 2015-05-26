c94002cd63af88ebc16f6a6b7262d30300a10ad9
Alan Foster <alan@shopkeep.com>
2015-05-26 22:22:18 +0100

    Add scss compilation to gulp

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module RailsBlog
   class Application < Rails::Application
     config.assets.paths << Rails.root.join('public', 'assets', 'javascripts')
+    config.assets.paths << Rails.root.join('public', 'assets', 'stylesheets')
     config.assets.paths << Rails.root.join('node_modules', 'highlight.js', 'styles')
   end
 end
--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,24 @@ var plugins = require('gulp-load-plugins')();
 var settings = require('./config/js');
 
 gulp.task('assets:javascript', function () {
-  return gulp.src(settings.build.js + '/**/*')
+  return gulp.src(settings.build.js)
              .pipe(plugins.plumber())
              .pipe(plugins.webpack(settings.webpack))
              .pipe(gulp.dest(settings.build.target + '/javascripts'));
 });
 
-gulp.task('assets', ['assets:javascript']);
+gulp.task('assets:stylesheets', function() {
+  return gulp.src(settings.build.stylesheets.index)
+             .pipe(plugins.plumber())
+             .pipe(plugins.sourcemaps.init())
+             .pipe(plugins.sass({ indentedSyntax: false }).on('error', plugins.sass.logError))
+             .pipe(plugins.autoprefixer())
+             .pipe(plugins.sourcemaps.write())
+             .pipe(plugins.rename('application.css'))
+             .pipe(gulp.dest(settings.build.target + '/stylesheets'));
+});
+
+gulp.task('assets', ['assets:javascript', 'assets:stylesheets']);
 gulp.task('build', ['assets']);
 
 gulp.task('clean', function (cb) {
@@ -21,7 +32,10 @@ gulp.task('clean', function (cb) {
 });
 
 gulp.task('watch', ['clean', 'build'], function() {
-  gulp.watch([settings.build.js + '/**/*'], ['build'])
+  gulp.watch([
+    settings.build.js,
+    settings.build.stylesheets.watch
+  ], ['build'])
 });
 
 gulp.task('default', ['clean', 'build']);
